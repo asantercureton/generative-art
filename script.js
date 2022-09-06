eee6d142d8f91afd6977f7f0cb43d4ee0689300c
Asante Cureton <asantercureton@gmail.com>
2022-09-06 14:14:45 -0400

    Added functionality to randomize tree branchWidth, branch color, branch width, as well as rotate. Background changes colors as well

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const generateBtn = document.querySelector('.generate-btn');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
+let curve;
 
 function drawTree(startX, startY, len, angle, branchWidth, color1, color2) {
     ctx.beginPath();
@@ -10,20 +11,52 @@ function drawTree(startX, startY, len, angle, branchWidth, color1, color2) {
     ctx.strokeStyle = color1;
     ctx.fillStyle = color2;
     ctx.lineWidth = branchWidth;
+    ctx.shadowBlur = 15;
+    ctx.shadowColor = color2;
     ctx.translate(startX, startY);
     ctx.rotate(angle * Math.PI/180);
     ctx.moveTo(0,0);
-    ctx.lineTo(0, -len);
+    // ctx.lineTo(0, -len);
+    if (angle) {
+        ctx.bezierCurveTo(20, -len/2, 20, -len/2, 0, -len);
+    } else {
+        ctx.bezierCurveTo(20, -len/2, -20, -len/2, 0, -len);
+    }
+        
     ctx.stroke();
 
-    if (len < 10) {
+    if (len < 8) {
+        // leaves
+        ctx.beginPath();
+        ctx.arc(0, -len, 10, 0, Math.PI/2);
+        ctx.fill();
         ctx.restore();
         return;
     }
+    curve = (Math.random() * 10) + 10;
 
-    drawTree(0, -len, len * 0.75, angle + 5, branchWidth);
-    drawTree(0, -len, len * 0.75, angle - 5, branchWidth);
+    drawTree(0, -len, len * 0.75, angle + curve, branchWidth * 0.9);
+    drawTree(0, -len, len * 0.75, angle - curve, branchWidth * 0.4);
 
     ctx.restore();
 }
-drawTree(canvas.width/2, canvas.height - 80, 120, 0, 2, 'aqua', 'green');
+drawTree(canvas.width/2, canvas.height - 80, 120, 0, 20, 'blue', 'orangered');
+
+function generateRandomTree() {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    // startX, startY, len, angle, branchWidth, color1, color2
+    let centerPointX = canvas.width/2;
+    let len = Math.floor((Math.random() * 20) + 100);
+    let angle = 0;
+    let branchWidth = (Math.random() * 70) + 1;
+    let color1 = 'rgb(' + Math.random() * 255 + ', ' + Math.random() * 255 + ', ' + Math.random() * 255 + ')';
+    let color2 = 'rgb(' + Math.random() * 255 + ', ' + Math.random() * 255 + ', ' + Math.random() * 255 + ')';
+    let color3 = 'rgb(' + Math.random() * 255 + ', ' + Math.random() * 255 + ', ' + Math.random() * 255 + ')';
+
+    generateBtn.style.background = color1;
+    generateBtn.style.color = canvas.style.background = color3;
+
+    drawTree(centerPointX, canvas.height - 80, len, angle, branchWidth, color1, color2);
+}
+
+generateBtn.addEventListener('click', generateRandomTree);
